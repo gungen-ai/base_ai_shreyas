535be65a6ec4db96ef96c9152a006787256bb2aa
Gunjan Bhardwaj <gunjanbhardwaj17@gmail.com>
2026-03-28 17:21:17 -0500

    Make /api/query public for widget access

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-up(.*)',
   '/api/inngest',
   '/api/webhooks(.*)',
+  '/api/query',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
